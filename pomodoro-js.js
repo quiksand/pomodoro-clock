3da4538d8aed74de505d3df55914517aa10b57fa
Cody Schindler <admin@codyschindler.com>
2015-10-27 16:15:40 -0700

    initial commit

---
+++ pomodoro-js.js
@@ -0,0 +1,19 @@
+/**
+ * FILENAME:    pomodoro-js.js
+ * DESCRIPTION: This is the javascript for codyschindler.com/fcc/pomodoro.
+ * REQS:        jQuery
+ * VERISON:     1.0.0
+ * UPDATED:     2015-10-27
+ * WRITTEN BY:  Cody Schindler (quiksand)
+ * ORGANIZATION: n/a
+ * CHANGELOG:  
+ * 1.0.0 (2015-10-27): Creation
+**/
+
+$(document).ready(function(){
+
+
+
+
+
+}); //document.ready()
